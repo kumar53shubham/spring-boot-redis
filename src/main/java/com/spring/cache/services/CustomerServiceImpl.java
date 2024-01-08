5939488b224de035fc8c6c4d195edce36bdbf76e
Shubham-Kumar-cmd <kumar53shubham@gmail.com>
2024-01-08 15:29:55 +0530

    spring redis as Cache

--- src/main/java/com/spring/cache/services/CustomerServiceImpl.java
+++ src/main/java/com/spring/cache/services/CustomerServiceImpl.java
@@ -3,13 +3,20 @@ package com.spring.cache.services;
 import com.spring.cache.hash.Customer;
 import com.spring.cache.repo.CustomerDao;
 import com.spring.cache.repo.CustomerRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.StreamSupport;
 
 @Service
+@CacheConfig(cacheNames = "customers")
+@Slf4j
 public class CustomerServiceImpl implements CustomerService{
 
     @Autowired
@@ -19,13 +26,17 @@ public class CustomerServiceImpl implements CustomerService{
     private CustomerRepository customerRepository;
 
     @Override
+    @CachePut(key = "#customers.id")
     public Customer saveCustomer(Customer customer) {
+        log.info("CustomerServiceImpl::saveCustomer() talking to DB");
 //        return customerDao.addCustomer(customer);
         return customerRepository.save(customer);
     }
 
     @Override
+    @CachePut(key = "#id")
     public Customer updateCustomer(int id, Customer customer) {
+        log.info("CustomerServiceImpl::updateCustomer() talking to DB");
 //        return customerDao.updateCustomer(id, customer);
         Customer existingCustomer = customerRepository.findById(id).orElseThrow(() -> new RuntimeException("Customer not found!!"));
         existingCustomer.setFirstName(customer.getFirstName());
@@ -37,7 +48,9 @@ public class CustomerServiceImpl implements CustomerService{
     }
 
     @Override
+    @Cacheable
     public List<Customer> getAllCustomers() {
+        log.info("CustomerServiceImpl::getAllCustomers() talking to DB");
 //        return customerDao.getAllCustomers();
 
 //        convert iterable to collection java 8 feature
@@ -46,13 +59,17 @@ public class CustomerServiceImpl implements CustomerService{
     }
 
     @Override
+    @Cacheable(key = "#id")
     public Customer getCustomer(int id) {
+        log.info("CustomerServiceImpl::getCustomer() talking to DB");
 //        return customerDao.getCustomer(id);
         return customerRepository.findById(id).get();
     }
 
     @Override
+    @CacheEvict(key = "#id")
     public String deleteCustomer(int id) {
+        log.info("CustomerServiceImpl::deleteCustomer() talking to DB");
 //        return customerDao.deleteCustomer(id);
         Customer customer = customerRepository.findById(id).orElseThrow(() -> new RuntimeException("Customer not found!!"));
         customerRepository.delete(customer);
